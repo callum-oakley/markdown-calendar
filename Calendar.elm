c88fa65e771acb78fc300a8de7bb93b531cad930
Callum Oakley <c.oakley108@gmail.com>
2016-08-20 01:20:20 +0100

    basic functionality taking shape

--- Calendar.elm
+++ Calendar.elm
@@ -1,134 +1,47 @@
-module Calendar exposing (..)
+module Calendar exposing (display)
 
 
-import Date exposing (Month(..))
-import Date.Extra as Date
+import Date
+import Date.Extra
+import List exposing (..)
 import List.Extra
-import Time
+import Maybe
 
 
--- displayCalendar : Date.Date -> Date.Date -> String
--- displayCalendar from to =
+display : Date.Date -> Date.Date -> String
+display from to =
+  Date.Extra.range Date.Extra.Day 1 from to
+    |> groupByMonth
+    |> List.map formatGroup
+    |> concatenate
 
 
-
-dateRange : Date.Date -> Date.Date -> List Date.Date
-dateRange from to =
+groupByMonth : List Date.Date -> List (Date.Date, List Date.Date)
+groupByMonth dates =
   let
-    maybeNext date =
-      if next date == to then
-        Nothing
-      else
-        Just (next date)
+    extractHeader group =
+      Maybe.withDefault
+        (Date.Extra.fromCalendarDate 1994 Date.Feb 2)
+        (head group)
 
   in
-    List.Extra.iterate maybeNext from
+    List.Extra.groupWhile (\x y -> Date.month x == Date.month y) dates
+      |> List.map (\group -> (extractHeader group, group))
 
 
-next : Date.Date -> Date.Date
-next =
-    Date.fromTime << (+) (24 * Time.hour) << Date.toTime
-
-
-sayDate : Date.Date -> String
-sayDate date =
+formatGroup : (Date.Date, List Date.Date) -> (String, List String)
+formatGroup (header, dates) =
   let
-    dayOfWeek =
-      sayDayOfWeek <| Date.dayOfWeek date
+    formatHeader =
+      Date.Extra.toFormattedString "### MMMM y\n\n"
 
-    day =
-      sayDay <| Date.day date
-
-    month =
-      sayMonth <| Date.month date
-
-    year =
-      toString <| Date.year date
+    formatDate =
+      Date.Extra.toFormattedString "#### EEEE ddd MMMM y\n\n"
 
   in
-    dayOfWeek ++ " " ++ day ++ " " ++ month ++ " " ++ year
+    (formatHeader header, List.map formatDate dates)
 
 
-sayDayOfWeek : Date.Day -> String
-sayDayOfWeek dayOfWeek =
-  case dayOfWeek of
-    Date.Mon ->
-      "Monday"
-
-    Date.Tue ->
-      "Tuesday"
-
-    Date.Wed ->
-      "Wednesday"
-
-    Date.Thu ->
-      "Thursday"
-
-    Date.Fri ->
-      "Friday"
-
-    Date.Sat ->
-      "Saturday"
-
-    Date.Sun ->
-      "Sunday"
-
-
-sayDay : Int -> String
-sayDay day =
-  let
-    suffix =
-      if day == 1 || day == 21 || day == 31 then
-        "st"
-
-      else if day == 2 || day == 22 then
-        "nd"
-
-      else if day == 3 || day == 23 then
-        "rd"
-
-      else
-        "th"
-
-  in
-    toString day ++ suffix
-
-
-sayMonth : Date.Month -> String
-sayMonth month =
-  case month of
-    Date.Jan ->
-      "January"
-
-    Date.Feb ->
-      "February"
-
-    Date.Mar ->
-      "March"
-
-    Date.Apr ->
-      "April"
-
-    Date.May ->
-      "May"
-
-    Date.Jun ->
-      "June"
-
-    Date.Jul ->
-      "July"
-
-    Date.Aug ->
-      "August"
-
-    Date.Sep ->
-      "September"
-
-    Date.Oct ->
-      "October"
-
-    Date.Nov ->
-      "November"
-
-    Date.Dec ->
-      "December"
+concatenate : List (String, List String) -> String
+concatenate =
+  foldr (\(header, dates) acc -> header ++ foldr (++) "" dates ++ acc) ""
--- Main.elm
+++ Main.elm
@@ -1,36 +1,80 @@
 import Calendar
+import Date
+import Date.Extra
 import Html exposing (..)
+import Html.App as App
 import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+import Result
 
 
--- main =
---   App.program
---     { init = init
---     , view = view
---     , update = update
---     , subscriptions = subscriptions
---     }
+main =
+  App.beginnerProgram
+    { model = model
+    , view = view
+    , update = update
+    }
+
 
 
 -- MODEL
 
+
 type alias Model =
-  { startDate : Date
-  , endDate : Date
-  , baseHeaderLevel : Int
-  , monthOverviews : Bool
+  { from : Result String Date.Date
+  , to : Result String Date.Date
   }
 
 
+model : Model
+model =
+  Model
+    (Ok <| Date.Extra.fromCalendarDate 2017 Date.Jan 1)
+    (Ok <| Date.Extra.fromCalendarDate 2018 Date.Jan 1)
 
-
--- init : (Model, Cmd Msg)
--- init =
---   (Model)
 
 
 -- UPDATE
 
 
+type Msg
+  = From String
+  | To String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    From newFrom ->
+      { model | from = Date.fromString newFrom }
+
+    To newTo ->
+      { model | to = Date.fromString newTo }
+
+
 
 -- VIEW
+
+
+view : Model -> Html Msg
+view ({ from, to } as model) =
+  div []
+    [ input [ type' "text", placeholder "from (yyyy-mm-dd)", onInput From ] []
+    , input [ type' "text", placeholder "to (yyyy-mm-dd)", onInput To ] []
+    , p [] []
+    , span [ style [ ("white-space", "pre-wrap") ] ] [text (calendarView model)]
+    ]
+
+
+calendarView : Model -> String
+calendarView { from, to } =
+  let
+    calendarOrErr = Result.map2 Calendar.display from to
+
+  in
+    case calendarOrErr of
+      Ok calendar ->
+        calendar
+
+      Err _ ->
+        "Please enter two dates conforming to ISO 8601."
